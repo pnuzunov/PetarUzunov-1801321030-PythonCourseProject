f514a1921fae26790169d18e6c336a8dba2aefe5
pnuzunov <pnuzunov@gmail.com>
2021-03-07 11:48:53 +0200

    Update 07-03-2021

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import ant_colony_simulator as acs
+import argparse
 
 if __name__ == '__main__':
-    acs.run('./acs_input.json')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_input')
+    args = parser.parse_args()
+
+    if(args.file_input):
+        acs.run(args.file_input)
+    else:
+        acs.run('./acs_input.json')
 
 # input("Press any key to continue...")
